390d3f69eca7f3b2f29f4a4539a5ab2450331abc
jared <jared@SAM.PK5001Z>
2020-02-22 10:24:27 -0600

    Finished test scores and grade programming challenge

--- src/main/java/TestScoresAndGrade.java
+++ src/main/java/TestScoresAndGrade.java
@@ -1,13 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author jared
- */
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class TestScoresAndGrade {
-    
+
+    public static DecimalFormat df = new DecimalFormat("0.00");
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Please enter your first test score");
+
+        double firstTestScore = keyboard.nextDouble();
+
+        System.out.println("Please enter your second test score");
+
+        double secondTestScore = keyboard.nextDouble();
+
+        System.out.println("Please enter your third test score");
+
+        double thirdTestScore = keyboard.nextDouble();
+
+        double averageTestScore = (firstTestScore + secondTestScore + thirdTestScore) / 3;
+
+        System.out.println("The average of your test scores is: " + df.format(averageTestScore));
+
+        if (averageTestScore >= 90) {
+            System.out.println("Your letter grade is an A");
+        } else if (averageTestScore >= 80) {
+            System.out.println("Your letter grade is a B");
+        } else if (averageTestScore >= 70) {
+            System.out.println("Your letter grade is a C");
+        } else if (averageTestScore >= 60) {
+            System.out.println("Your letter grade is a D");
+        } else {
+            System.out.println("Your letter grade is an F");
+        }
+
+    }
 }
